f39ec403a6034f77b51cfef3dacfa51718f2a49e
HEMAT KHAN <88194852+HemtKhan@users.noreply.github.com>
2022-10-18 11:59:07 +0430

    Update HEMAT.py

--- HEMAT.py
+++ HEMAT.py
@@ -1,2 +1,8 @@
-import M4
-M4.M4lmand()
+import platform
+bit=platform.architecture()[0]
+if bit =='64bit':
+    import M4
+    Nida()
+else:
+    print('Sorry device or system not support this tools')
+    exit()
